19731a35967d778db00201c1b17a158b6a50c59d
AresOsredkar <aresosredkar1337@gmail.com>
2023-01-14 19:51:34 +0100

    dsadsdsadasdas

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+Card.propTypes = {
+
+};
+
+export default Card;
